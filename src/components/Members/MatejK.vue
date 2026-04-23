f0c66c40192f3b3f8e80ec6b534e75ac9d9de7d5
TadeasDitte <tadeas.ditte@gmail.com>
2026-04-23 10:04:40 +0200

    Merge branch 'main' of github.com:NNCYB/SucklessWeb

--- src/components/Members/MatejK.vue
+++ src/components/Members/MatejK.vue
@@ -13,19 +13,19 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge'
-import { Brush } from 'lucide-vue-next'
+import { Component } from 'lucide-vue-next'
 </script>
 
 <template>
 
   <Card>
     <CardHeader>
-        <div class="flex flex-row flex-wrap items-center gap-12">
-            <Avatar>
-            <AvatarImage src="" alt="@mk" />
-            <AvatarFallback>MK</AvatarFallback>
-            </Avatar>
-        </div>
+      <div class="flex flex-row flex-wrap items-center gap-12">
+        <Avatar>
+          <AvatarImage src="" alt="@mk" />
+          <AvatarFallback>MK</AvatarFallback>
+        </Avatar>
+      </div>
       <CardTitle>Matej Kováč</CardTitle>
       <CardDescription>Design Robota</CardDescription>
     </CardHeader>
@@ -33,15 +33,13 @@ import { Brush } from 'lucide-vue-next'
       <p>Cakame na spravu</p>
     </CardContent>
     <CardFooter>
-        <div class="flex w-full flex-wrap gap-2">
-        <Badge
-          variant="outline"
-          class="border-slate-500/25 bg-slate-500/10 text-slate-200 dark:border-slate-400/25 dark:bg-slate-400/10 dark:text-slate-100"
-        >
-                <Brush />
-                Vlastne tam ani nebol
-            </Badge>
-        </div>
+      <div class="flex w-full flex-wrap gap-2">
+        <Badge variant="outline"
+          class="border-slate-500/25 bg-slate-500/10 text-slate-200 dark:border-slate-400/25 dark:bg-slate-400/10 dark:text-slate-100">
+          <Brush />
+          Design robota
+        </Badge>
+      </div>
     </CardFooter>
   </Card>
 
